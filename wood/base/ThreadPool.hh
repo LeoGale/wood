ad016cc1ef9b12eb6090c387abf9bd037b072ee1
LeoGale <leoogale@gmail.com>
2019-09-11 15:36:50 +0800

    Update ThreadPool.hh
    fix compile error

--- wood/base/ThreadPool.hh
+++ wood/base/ThreadPool.hh
@@ -6,169 +6,143 @@
 #include <type_traits>
 #include <functional>
 #include <deque>
+#include <iterator>
 
-namespace wood {
+namespace wood 
+{
 
-class Job {
+using Task = std::function<void()>;
+
+class Worker 
+{
 public:
-	//template<class F, typename... Args>
-	//Job(F && f, Args && ... args)
-	//:task_(std::bind(std::forward<F>(f), std::forward<Args>(args)...))
-	//{
-	//}
-	
-	//using R= typename std::result_of<F(Args...)>::type;
+    Worker(std::uint64_t id)
+    :isStopped_(false),
+    id_(id)
+    {
+        thread_ = std::thread(&Worker::run, this);//FIXME safe ?
+    }
 
-	//std::future<R> get_future()
-	//{
-		//return task_.get_future();
-	//}
-	template<typename T>
-	Job(std::packaged_task<T> && task)
-	:task_(std::forward<std::packaged_task<T>>(task))
-	{
-	}
+    ~Worker()
+    {
+        if(!isStopped_)
+        {
+            //FIXME callonce
+            stop();
+        }
+    }
 
-	void operator()()
-	{
-		task_();
-	}
+    void stop()
+    {
+        isStopped_ = true;
+        if(thread_.joinable())
+        {
+            thread_.join();
+        }
+    }
+
+    void assign(Task && task)
+    {
+        std::lock_guard<std::mutex> lock(mutex_);
+        tasks_.push_back(std::forward<Task>(task));
+        cond_.notify_one();
+    }
+
+    void run()
+    {
+        std::cout <<"worker id " << (int)id_ << " in thread " << thread_.get_id() << std::endl;
+        while(!isStopped_)
+        {
+            std::deque<Task> tasks;
+            {
+                std::unique_lock<std::mutex> lock(mutex_);
+            
+                cond_.wait(lock, [this](){return !tasks_.empty();});
+                
+                tasks = std::move(tasks_);
+            }
+               
+            std::cout <<"worker id " << (int)id_ << " in thread " << thread_.get_id() <<" task size:" << tasks.size()<< std::endl;
+
+            for(auto & task : tasks)
+            {
+                task();
+            }
+        }
+    }
+
 private:
-	std::packaged_task<T> task_;
-       	//Task task_;
-};
-
-class Worker {
-	using FetchJobsFunc = std::function<void(std::deque<Job>&, std::size_t)>; 
-public:
-	Worker(std::uint64_t id)
-	:isStopped_(false),
-	id_(id)
-	{
-		thread_ = std::thread(&Worker::run, this);//FIXME safe ?
-	}
-
-	~Worker()
-	{
-		if(!isStopped_)
-		{
-			stop();
-		}
-	}
-
-	void stop()
-	{
-		isStopped_ = true;
-		if(thread_.joinable())
- 		{
-			thread_.join();
-		}
-	}
-
-	void assign(Job && job)
-	{
-		jobs_.push_back(std::forward<Job>(job));
-	}
-
-	void run()
-	{
-		while(!isStopped_)
-		{
-		if(!jobs_.empty())
-		{
-			for(auto & job : jobs_)
-			{
-				job();
-			}
-		}
-
-		std::deque<Job> jobs;
-		fetchJobs_(jobs, 1);
-
-		if(jobs_.empty())
-		{
-			//TODO sleep a little while
-			std::this_thread::yield();
-		}
-		else 
-		{
-			for(auto & job : jobs)
-			{
-				//Fix If it is time_consumed job, 
-				job();
-			}
-		}
-		}
-	}
-
-	void setFetchJobsHandler(FetchJobsFunc handle)
-	{
-		fetchJobs_ = std::move(handle);
-	}
-private:
-	bool isStopped_;
-	std::uint64_t id_;
-	std::deque<Job> jobs_;
-	std::thread thread_;
-	FetchJobsFunc fetchJobs_;
+    std::mutex mutex_;
+    std::condition_variable cond_;
+    bool isStopped_;
+    std::uint64_t id_;
+    std::deque<Task> tasks_;
+    std::thread thread_;
 };
 
 class ThreadPool {
 public:
-	ThreadPool(std::size_t size)
-	{
-		for(std::size_t i = 0; i < size; i++)
-		{
-			workers_.emplace_back(i)
-			workers_.at(i).setFetchJobsHandler(std::bind(&ThreadPool::fetchJob, this, std::placeholders::_1, std::placeholders::_2));
-		}
-	}
+    ThreadPool(std::size_t size)
+    {
+        for(std::size_t i = 1; i <= size; i++)
+        {
+            workers_.emplace_back(i);
+        }
+    }
 
-	~ThreadPool()
-	{
-		stop();
-	}
+    ~ThreadPool()
+    {
+        //FIXME check flag
+        stop();
+    }
 
-	void stop()
-	{
-		for(auto & worker : workers_)
-		{
-			worker.stop();
-		}
-	}
+    void stop()
+    {
+        for(auto & worker : workers_)
+        {
+            worker.stop();
+        }
+    }
 
-	template<class F, typename ... Args>
-	auto deliver( F f, Args... args)->decltype(std::future<std::result_of<F(Args...)>::type>)
-	{
-		std::lock_guard<std::mutex> lock(mutex_);
-		jobs_.emplace_back(f, args...);
-		return jobs_.back().get_future();
-	}
+    template<class F, typename ... Args>
+    std::future<typename std::result_of<F(Args...)>::type> deliver( F && f, Args && ... args)
+    {
+        return do_deliver(getWorkerIndex(), std::forward<F>(f), std::forward<Args>(args)...); 
+    }
 
-	template<class F, typename ... Args>
-	auto deliver(uint64_t index, F f, Args... args)->decltype(std::future<typename std::result_of<F(Args...)>::type>)
-	{
-		if(workers_.empty())
-		{
-			std::runtime_error("There is no workers running in the pool");
-		}
-		std::packaged_task<typename std::result_of<F(Args...)>::type> task(f);
-		auto result = job.get_future();
-		workers_.at(index%sizeof(wokers_)).assign(task, args...);
-		return result;
-	}
-
-	void fetchJob(std::deque<Job> & jobs, size_t size)
-	{
-		std::lock_guard<std::mutex> lock(mutex_);
-		std::swap_ranges(jobs_.begin(), jobs_.begin()+size, jobs.begin());
-		jobs_.erase(jobs_.begin(), jobs_.begin()+size);
-		//jobs_.pop_front();
-	}
+    template<class F, typename ... Args>
+    std::future<typename std::result_of<F(Args...)>::type> deliver(uint64_t index, F && f, Args &&... args)
+    {
+        return do_deliver(index, std::forward<F>(f), std::forward<Args>(args)...);
+    }
 
 private:
-	std::deque<Worker> workers_;
-	std::mutex mutex_;
-	std::deque<Job> jobs_;
+    template <class F, typename ... Args>
+    std::future<typename std::result_of<F(Args...)>::type> do_deliver(uint64_t index, F && f, Args && ... args)
+    {
+        std::lock_guard<std::mutex> lock(mutex_);
+        if(workers_.empty())
+        {
+            std::runtime_error("There is no workers running in the pool");
+        }
+        using ReturnType = typename std::result_of<F(Args...)>::type;
+        
+        using Packeged_Task = std::packaged_task<ReturnType()>;
+        
+        std::shared_ptr<Packeged_Task> task= std::make_shared<Packeged_Task>(std::bind(f, args...));
+
+        workers_.at(index%workers_.size()).assign([task](){(*task)();});
+        return task->get_future();
+    }
+    
+    uint64_t getWorkerIndex()
+    {
+        //FIXME add more strategies
+        static uint64_t tasksNum = 1;
+        tasksNum++;
+    }
+
+    std::deque<Worker> workers_;
+    std::mutex mutex_;
 };
 }
