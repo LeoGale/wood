8eef60cbeb006c732f6eadfd48d917e264bae401
LeoGale <leoogale@gmail.com>
2019-09-11 15:38:29 +0800

    Update testThreadPool.cc
    add three test cases

--- examples/threadpool/testThreadPool.cc
+++ examples/threadpool/testThreadPool.cc
@@ -6,6 +6,21 @@
 int main(int argc, char* argv[])
 {
 	wood::ThreadPool threadPool(3);
-	auto future = threadPool.deliver([](){return 1+3;});
+	auto future2 = threadPool.deliver([](){std::cout <<"Just say hello" << std::endl;throw std::runtime_error("test Exception");});
+
+	auto future = threadPool.deliver(0, [](){return 1+3;});
 	std::cout <<"result " << future.get() << std::endl;
+
+	auto future1 = threadPool.deliver([](double a, double b){return a+b;}, 3.01, 4.09);
+	std::cout <<"result1 " << future1.get() << std::endl;
+    
+
+    	try
+    	{
+		future2.get();
+    	}
+    	catch(std::exception& e)
+    	{
+    		std::cout <<"catch exception " << e.what() << std::endl;
+    	}
 }
